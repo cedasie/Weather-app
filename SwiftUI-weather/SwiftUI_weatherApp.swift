63cbd4514edf8868dd61186c8da994f779d5e019
cedasie <conquete.asie@gmail.com>
2024-06-13 08:23:16 +0200

    Initial Commit

---
+++ SwiftUI-weather/SwiftUI_weatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_weatherApp.swift
+//  SwiftUI-weather
+//
+//  Created by Ced on 13/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_weatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
